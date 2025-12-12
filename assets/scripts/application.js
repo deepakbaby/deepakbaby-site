5dac257cca4e8ab34369ba6bd86b1761254ba19d
Deepak Baby <deepakbaby@Deepaks-MacBook-Pro.local>
2025-12-12 14:23:24 +0100

    cleanup

--- assets/scripts/application.js
+++ assets/scripts/application.js
@@ -3,8 +3,9 @@ import 'bootstrap'
 import '@fortawesome/fontawesome-free/js/all'
 import feather from 'feather-icons'
 
-// Minimal application.js to bypass build errors
-// We can add back features/sections if needed
-console.log('Custom application.js loaded');
+import './core'
+import './features'
+import './sections'
+import './pages'
 
 feather.replace();
